7960559751dbe1dc1765fef1cdf9b1f54e0e2cde
taddeoneri <taddeo.neri10@gmail.com>
2023-03-10 15:20:45 +0100

    style

--- js/script.js
+++ js/script.js
@@ -9,15 +9,19 @@ const box = document.querySelector('.box');
 for(let i = 1; i <= 100; i++){
     if((i % 3 == 0) && (i % 5 == 0)){
         number = 'FizzBuzz';
+        background = 'bg-fizz-buzz';
     }else if(i % 5 == 0){
         number = 'Buzz';
+        background = 'bg-buzz';
     }else if(i % 3 == 0){
         number = 'Fizz';
+        background = 'bg-fizz';
     }else{
         number = i;
+        background = 'bg-n';
     }
     box.innerHTML += `
-        <div class="square d-flex center">
+        <div class="square d-flex center ${background}">
             <div class="content">${number}</div>
         </div>
     `;
